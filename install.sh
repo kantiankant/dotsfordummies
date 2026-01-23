e683473ddfaae412e821d952854d7216bf270a52
kantiankant <qwerasdnoob@gmail.com>
2026-01-23 19:38:38 +0800

    Automated process, nothing has changed

---
+++ install.sh
@@ -0,0 +1,11 @@
+!#/bin/zsh
+
+mv ~/dotsfordummies/fastfetch/ ~/.config/
+
+mv ~/dotsfordummies/ghostty/ ~/.config/
+
+mv ~/dotsfordummies/hypr/ ~/.config/
+
+mv ~/dotsfordummies/waybar/ ~/.config/
+
+mv ~/dotsfordummies/mako/ ~/.config
