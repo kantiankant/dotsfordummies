f2f5feea727ed991be6b86f75ca87eb65790ee1b
kantiankant <qwerasdnoob@gmail.com>
2026-01-25 21:21:35 +0800

    Automated process, nothing has changed

--- install.sh
+++ install.sh
@@ -8,4 +8,8 @@ cp -r ~/dotsfordummies/hypr/ ~/.config/
 
 cp -r ~/dotsfordummies/waybar/ ~/.config/
 
-cp -r ~/dotsfordummies/mako/ ~/.config
+cp -r ~/dotsfordummies/mako/ ~/.config/
+
+chmod +x ~/.config/hypr/scripts/wallpaper_switcher.sh
+
+chmod +x ~/dotsfordummies/scripts-for-mako/spotify_mako.sh
